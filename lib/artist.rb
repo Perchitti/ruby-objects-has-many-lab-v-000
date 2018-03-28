8900583d35fd917ed3e230ad1977c47b304359a4
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-28 19:26:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,8 @@ def songs
   @songs
 end
 
-def add_song_by_name(name)
-  song = Song.new(name)
+def add_song_by_name(song_name)
+  song = Song.new(song_name)
   @songs << song.artist = self
 end
 
